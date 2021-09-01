e16a8a22e8b0c9ae2d33d817f3bbb53e5a0395a7
Anjana Bajaj <bajajanjana1999@gmail.com>
2021-09-01 14:11:24 +0530

    Billing option added

--- src/components/Card/Pricing/Pricing.js
+++ src/components/Card/Pricing/Pricing.js
@@ -1,12 +1,14 @@
 import CalculatePrice from "./calculatePrice";
 import "./Pricing.css"
 const Pricing=(props)=>{
-    
+    // console.log(props.val);
+    // console.log("in pricing");
+    // console.log(props.toggleSwitchInput);
     return(
         <div className="pricing">
             <p>{props.val}K PAGEVIEWS</p>
             <div className="price_rate">
-            <CalculatePrice input_pageview={props.val}/>
+            <CalculatePrice input_pageview={props.val} switchState={props.toggleSwitchInput}/>
             <p> /month</p>
             </div>
         </div>
