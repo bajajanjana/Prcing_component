20133b676888ee6f8125218fa1c2565b9dc5f609
Anjana Bajaj <bajajanjana1999@gmail.com>
2021-09-01 10:52:50 +0530

    heading updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ const Header=()=>{
     return(
         <div className="header">
             <h4>Simple,traffic-based pricing</h4>
-            <p>Sign-up to our 30-day tria.No credit card required.</p>
+            <p><span className="line_change">Sign-up to our 30-day tria.</span>No credit card required.</p>
         </div>
     );
 }
